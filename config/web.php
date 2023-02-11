8e9be82bd9ccb3f0fdd6e34565e07721a4a030db
sergio762000 <sergio762000@mail.ru>
2023-02-11 10:52:03 +0500

    Добавили обработку JSON из тела запроса

--- config/web.php
+++ config/web.php
@@ -19,13 +19,16 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'Iii9VmaHk6018LV7HNO3DyLXHe7HR0zn',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser'
+            ],
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
         'user' => [
             'identityClass' => 'app\models\User',
-            'enableAutoLogin' => true,
+            'enableAutoLogin' => false,
         ],
         'errorHandler' => [
             'errorAction' => 'site/error',
