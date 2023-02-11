c128e7960048cd106f1a5f95b3f70e47dfc3715a
sergio762000 <sergio762000@mail.ru>
2023-02-11 18:51:30 +0500

    Add Token models

---
+++ models/Token.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "{{%token}}".
+ *
+ * @property integer $id
+ * @property integer $user_id
+ * @property integer $expired_at
+ * @property string $token
+ */
+class Token extends ActiveRecord
+{
+    public static function tableName()
+    {
+        return '{{%token}}';
+    }
+
+    /**
+     * @throws \yii\base\Exception
+     */
+    public function generateToken($expire)
+    {
+        $this->expired_at = $expire;
+        $this->token = Yii::$app->security->generateRandomString();
+    }
+
+    public function fields()
+    {
+        return [
+            'token' => 'token',
+            'expired' => function () {
+                return date(DATE_RFC3339, $this->expired_at);
+            }
+        ];
+    }
+}
